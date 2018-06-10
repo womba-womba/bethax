882180ea5cbb17a3e0102bcf168ff4926760992e
womba-womba <armin.zakeri@gmail.com>
2018-06-10 23:54:58 +0200

    aa

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
