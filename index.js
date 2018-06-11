4d73064f9ef3dc4fa6d07e1247fea9a2e865ad88
womba-womba <armin.zakeri@gmail.com>
2018-06-11 13:01:23 +0200

    a

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', function(req, res){
-   res.send("Hello world!!!!!");
-});
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
