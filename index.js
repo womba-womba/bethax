63cdd5bc31c14d90ee4568f4fb3b2142aa2a84d9
womba-womba <armin.zakeri@gmail.com>
2018-06-12 02:24:39 +0200

    added navbar

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const PORT = process.env.PORT || 5000;
 
 
 app.set('views', './views');
+app.use(express.static('./public'));
 app.set('view engine', 'pug');
 
+
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index')
 })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
